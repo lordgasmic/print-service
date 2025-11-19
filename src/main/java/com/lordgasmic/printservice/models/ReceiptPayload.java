56a1a62b057932c204145c61c26f3a8822cca0a8
lordgasmic <lordgasmic@lordgasmic.com>
2025-11-19 13:14:43 -0500

    Update and rename PrintPayload.java to ReceiptPayload.java

--- src/main/java/com/lordgasmic/printservice/models/ReceiptPayload.java
+++ src/main/java/com/lordgasmic/printservice/models/ReceiptPayload.java
@@ -8,7 +8,7 @@ import java.util.Map;
 
 @EqualsAndHashCode(callSuper = true)
 @Data
-public class PrintPayload extends Payload {
+public class ReceiptPayload extends Payload {
     private String message;
     private Map<String, List<String>> properties;
 }
