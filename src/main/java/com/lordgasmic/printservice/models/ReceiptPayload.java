d1f77894c220175c623dee643f7353b0298eaab5
Nick Wellman <wellman.nicholas@gmail.com>
2025-11-19 23:53:56 -0500

    update properties type

--- src/main/java/com/lordgasmic/printservice/models/ReceiptPayload.java
+++ src/main/java/com/lordgasmic/printservice/models/ReceiptPayload.java
@@ -3,12 +3,11 @@ package com.lordgasmic.printservice.models;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
-import java.util.List;
 import java.util.Map;
 
 @EqualsAndHashCode(callSuper = true)
 @Data
 public class ReceiptPayload extends Payload {
     private String message;
-    private Map<String, List<String>> properties;
+    private Map<String, String[]> properties;
 }
